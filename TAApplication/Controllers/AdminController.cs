938c46f0c1eb2e848a3180518d6b98d1f6c2a243
WENLIN LI <u1327012@umail.utah.edu>
2022-09-29 20:45:20 -0600

    Add Comment

--- TAApplication/Controllers/AdminController.cs
+++ TAApplication/Controllers/AdminController.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿/**
+* Author:    Xia Li
+* Partner:   Wenlin Li
+* Date:      09/29/2022
+* Course:    CS 4540, University of Utah, School of Computing
+* Copyright: CS 4540 and Xia Li and Wenlin Li - This work may not be copied for use in Academic Coursework.
+*
+* I, Xia Li and Wenlin Li, certify that I wrote this code from scratch and did 
+* not copy it in part or whole from another source.  Any references used 
+* in the completion of the assignment are cited in my README file and in
+* the appropriate method header.
+*
+* File Contents
+*
+* This css file controller for admin.
+*/
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TAApplication.Areas.Data;
--- TAApplication/Services/EmailSender.cs
+++ TAApplication/Services/EmailSender.cs
@@ -1,4 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿/**
+* Author:    Xia Li
+* Partner:   Wenlin Li
+* Date:      09/29/2022
+* Course:    CS 4540, University of Utah, School of Computing
+* Copyright: CS 4540 and Xia Li and Wenlin Li - This work may not be copied for use in Academic Coursework.
+*
+* I, Xia Li and Wenlin Li, certify that I wrote this code from scratch and did 
+* not copy it in part or whole from another source.  Any references used 
+* in the completion of the assignment are cited in my README file and in
+* the appropriate method header.
+*
+* File Contents
+*
+* This css file controller for home.
+*/
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using SendGrid;
 using SendGrid.Helpers.Mail;
